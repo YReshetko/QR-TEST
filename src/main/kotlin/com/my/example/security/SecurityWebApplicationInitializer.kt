fb07e4d0653f2a803e31296ea8cc9544372647a3
lissgrad@gmail.com <yrashetska@gmail.com>
2018-01-23 21:20:04 +0200

    Update

--- src/main/kotlin/com/my/example/security/SecurityWebApplicationInitializer.kt
+++ src/main/kotlin/com/my/example/security/SecurityWebApplicationInitializer.kt
@@ -1,6 +1,4 @@
 package com.my.example.security
 
 import org.springframework.security.web.context.*
-
-
 class SecurityWebApplicationInitializer : AbstractSecurityWebApplicationInitializer(WebSecurityConfig::class.java)
